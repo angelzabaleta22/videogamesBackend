9964b857749ed612f658c3f8a11328c7094bfb7a
Angel Zabaleta Torrez <angelzabaleta08@hotmail.com>
2023-04-03 10:29:54 -0500

    14 commit

--- index.js
+++ index.js
@@ -2,10 +2,9 @@ const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 require("dotenv").config();
 
-const port = process.env.PORT;
 // Syncing all the models at once.
 conn.sync({ alter: true }).then(() => {
-  server.listen(process.env.port, () => {
-    console.log("%s listening at", port); // eslint-disable-line no-console
+  server.listen(process.env.PORT, () => {
+    console.log("%s listening at", process.env.PORT); // eslint-disable-line no-console
   });
 });
